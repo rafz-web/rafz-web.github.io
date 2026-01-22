04b500a942b5cc4e64b13cb748c42d398992a660
rafz-web <arvansyahraffa193@gmail.com>
2026-01-22 21:13:45 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,20 @@
-// MENU
-const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
-
-if (menuBtn) {
-  menuBtn.onclick = () => {
-    menu.classList.toggle("hidden");
-  };
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("hidden");
 }
 
-// JAM
-const clock = document.getElementById("clock");
-if (clock) {
-  setInterval(() => {
-    clock.innerText = new Date().toLocaleTimeString();
-  }, 1000);
-}
-
-// KALKULATOR
-let display = document.getElementById("display");
-
-function press(value) {
-  display.value += value;
+function append(val) {
+  document.getElementById("display").value += val;
 }
 
 function clearDisplay() {
-  display.value = "";
+  document.getElementById("display").value = "";
 }
 
 function calculate() {
   try {
-    display.value = eval(display.value);
+    document.getElementById("display").value =
+      eval(document.getElementById("display").value);
   } catch {
-    display.value = "Error";
+    alert("Error perhitungan");
   }
 }
